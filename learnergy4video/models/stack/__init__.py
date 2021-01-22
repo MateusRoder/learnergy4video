cce3e784a79bd399b8c6d392591448282cb480ce
Mateus Roder <45532871+MateusRoder@users.noreply.github.com>
2021-01-22 15:15:34 -0300

    Update __init__.py

--- learnergy4video/models/stack/__init__.py
+++ learnergy4video/models/stack/__init__.py
@@ -6,3 +6,4 @@ from learnergy4video.models.stack.DBM import DBM
 from learnergy4video.models.stack.conv_dbn import CDBN
 from learnergy4video.models.stack.spec_conv_dbn import SpecCDBN
 from learnergy4video.models.stack.spec_dbm import SpecDBM
+from learnergy4video.models.stack.spec_dbn import SpecDBN
